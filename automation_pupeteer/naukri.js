f23f97ddf8de0f8dd5189f6a6f5deb851172357b
Mansi Jain <mjain1996@gmail.com>
2024-07-23 21:56:01 +0530

    trying out the puppeteer library for automation in js

---
+++ automation_pupeteer/naukri.js
@@ -0,0 +1,92 @@
+const puppeteer = require('puppeteer');
+
+( async () => { const browser = await puppeteer.launch({headless:false, defaultViewport : null});
+const page = await browser.newPage();
+
+await page.goto('https://naukri.com');
+
+await page.waitForTimeout(5000);
+//console.log(page.locator('a #login_Layer'));
+//await page.locator('a #login_Layer').click(); //not working
+//const login = 
+await page.$('#login_Layer')
+//console.log("found the anchor");
+.then(async (el) => { 
+    await el.click()
+    .then(async () => {
+        console.log("clicked the anchor");
+        const selector = 'input[placeholder="Enter your active Email ID / Username"]';
+        //console.log("")
+        await page.waitForSelector(selector);
+        //const email = await page.$(selector);
+        console.log("got the email input");
+        await page.type(selector, 'mjain1996@gmail.com', { delay:100 });
+
+        const selector2 = 'input[type="password"]';
+        await page.waitForSelector(selector2);
+        await page.type(selector2, '$pupeteer#remote-job', { delay:100 });
+
+        await page.waitForTimeout(5000);
+
+        //const submit = 'button[type="submit"]';
+        const loginButton = await page.$('.loginButton')
+        
+        if(loginButton){
+            await loginButton.click();
+        }
+        else{
+            console.log("got an error while looking for login button");
+        }
+        await page.waitForNavigation(); 
+        //const completeProfile = await page.$('.view-profile-wrapper"]'); -  working
+        await page.waitForTimeout(5000);
+        //const completeProfile = 
+        await page.$('a[href="/mnjuser/profile').then(async (ele) => {
+            console.log(ele);
+            await ele.click().then(()=> console.log("Congrats you got the profile"))
+        }).catch((err)=> console.log("error while fetching the profile", err))
+
+        await page.waitForNavigation(); 
+
+        await page.waitForTimeout(5000);
+
+        await page.$('input[value="Update resume"]').then(async (ele) => {
+            await ele.click().then(() => {}).catch((err) => {})
+        })
+        .catch((err) => {
+            console.log("Error while fetching the input button field of update resume", err);
+        })
+        
+
+        // if(completeProfile){
+        //     completeProfile.click().then(()=> {
+        //         console.log("Congrats you got the profile");
+        //     })
+        // }
+        // else{
+        //     console.log("Error while fetching the profile");
+        // }
+        //await submit.click();
+
+
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+
+})
+.catch((err) => console.log(err));
+
+// if(login){
+//     await login.click()
+// }
+// else
+
+
+//then((el) => el.click()).catch((err) => console.log(err));
+
+//await page.type 
+
+//await browser.close();
+
+})()
